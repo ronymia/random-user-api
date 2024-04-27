5147c6665e04eba2cbc38c76c7c006648cd9e955
MD Rony Mia <mdronymia.javascript@gmail.com>
2024-04-27 12:19:35 +0600

    get a single user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,26 +1,79 @@
 const fs = require("fs");
 const path = require("path");
 
-const userJsonFile = path.join("public", "/randomUsers.json");
+const userJsonFile = path.join("public", "/randomUsers.json"); // import json file
 
+// get a single random user
+module.exports.getRandomUser = (req, res) => {
+    res.send("random user");
+};
+
+// get all user controller
 module.exports.getAllUser = (req, res, next) => {
     try {
-        const jsonData = fs.readFileSync(userJsonFile);
-        const parsedData = JSON.parse(jsonData);
-        const { limit } = req.query;
-        const result = (parsedData.slice(0, limit));
+        const usersJson = fs.readFileSync(userJsonFile); // get json data from file
+        const users = JSON.parse(usersJson); // convert to js object
+        const { limit } = req.query; // query limit
+        const result = (users.slice(0, limit)); // limited data
 
         res.status(200).json({
             success: true,
             data: result,
-            message: `successfully send ${limit} data`,
-            total: parsedData.length
+            message: `success`,
+            total: users.length
         })
     } catch (err) {
         console.error('Error reading file:', err);
         res.status(500).json({
-            error: true,
-            message: 'Internal Server Error'
+            success: false,
+            error: 'Internal Server Error'
         });
     }
-}
+};
+
+// get a single user controller
+module.exports.getSingleUser = (req, res) => {
+    try {
+        const usersJson = fs.readFileSync(userJsonFile); // get json data from file
+        const users = JSON.parse(usersJson); // convert to js object
+        const { id } = req.params; // user id from query params
+        // console.log( typeof id);
+        const result = users.find(user=> user.id === Number(id)); // get user by id
+        // console.log(result); 
+        if (!result) {
+            throw Error(`User not Exist given ID:${id}`);
+        }
+
+        res.status(200).json(result);
+    } catch (err) {
+        const errorMessage = err.message;
+        // console.log(errorMessage);
+        // console.warn('Error reading file:', err);
+        res.status(200).json({
+            success: false,
+            error: errorMessage
+        });
+    }
+};
+
+// post a single user 
+module.exports.saveSingleUser = (req, res) => {
+     res.send("save user");
+};
+
+// update a single user controller
+module.exports.updateSingleUser = (req, res) => {
+     res.send("update user");
+};
+
+
+// update bulk user controller
+module.exports.updateBulkUser = (req, res) => {
+     res.send("update bulk user");
+};
+
+
+// delete user controller
+module.exports.deleteUser = (req, res) => {
+     res.send("Delete a user");
+};
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -3,7 +3,134 @@ const userController = require("../../controllers/user.controller");
 
 const router = express.Router();
 
+/***************
+* @api {get} / route => user/random 
+* @apiDescription get a random user every time
+* @apiPermission anyone can  access
+*
+* @apiHeader {string} => user's access token not implement
+* @apiHeaderExample {json} Header-Example:
+*
+*
+*@apiSuccess {Object} response => user object
+*
+*@apiError {Unauthorized 401} => only authorized users can access this
+*@apiError {forbidden 403} => only only can access this
+*
+*************/
+router.get("/random", userController.getRandomUser);
+
+
+/***************
+* @api {get} / route => user/all 
+* @apiDescription get all users
+* @apiPermission anyone can  access
+*
+* @apiHeader {string} => user's access token not implement
+* @apiHeaderExample {json} Header-Example:
+*
+* @apiQuery {Number}   [limit] => limit=5
+*
+*@apiSuccess {Object[]} will get array of object
+*
+*@apiError {Unauthorized 401} => only authorized users can access this
+*@apiError {forbidden 403} => only only can access this
+*
+*************/
 router.get("/all", userController.getAllUser);
+
+/***************
+* @api {get} / user/:id
+* @apiDescription get single user by id
+* @apiPermission anyone can  access
+*
+* @apiHeader {string} => user's access token not implement
+* @apiHeaderExample {json} Header-Example:
+*
+* @apiParam {id}   [id=1] => id means user id
+*
+*@apiSuccess {Object} Response=> user object
+*
+*@apiError {Unauthorized 401} => only authorized users can access this
+*@apiError {forbidden 403} => only only can access this
+*
+*************/
+router.get("/:id" , userController.getSingleUser);
+
+
+/***************
+* @api {post} / user/save
+* @apiDescription save a single user
+* @apiPermission anyone can  access
+*
+* @apiHeader {string} => user's access token not implement
+* @apiHeaderExample {json} Header-Example:
+*
+*
+*@apiSuccess {Object} Response=> user object
+*
+*@apiError {Unauthorized 401} => only authorized users can access this
+*@apiError {forbidden 403} => only only can access this
+*
+*************/
+router.post("/save" , userController.saveSingleUser);
+
+
+/***************
+* @api {patch} / user/update
+* @apiDescription update a user by 
+* @apiPermission anyone can  access
+*
+* @apiHeader {string} => user's access token not implement
+* @apiHeaderExample {json} Header-Example:
+*
+* @apiParam {id}   [id=1] => id means user id
+*
+*@apiSuccess {Object} Response=> user object
+*
+*@apiError {Unauthorized 401} => only authorized users can access this
+*@apiError {forbidden 403} => only only can access this
+*
+*************/
+router.patch("/update" , userController.updateSingleUser);
+
+
+/***************
+* @api {patch} / user/bulk-update
+* @apiDescription update bulk user by array of id => [1,2,3]
+* @apiPermission anyone can  access
+*
+* @apiHeader {string} => user's access token not implement
+* @apiHeaderExample {json} Header-Example:
+*
+* @apiParam {id}   [id=1] => id means user id
+*
+*@apiSuccess {Object} Response=> user object
+*
+*@apiError {Unauthorized 401} => only authorized users can access this
+*@apiError {forbidden 403} => only only can access this
+*
+*************/
+router.patch("/bulk-update" , userController.updateBulkUser);
+
+
+/***************
+* @api {delete} / user/delete
+* @apiDescription delete user by user id
+* @apiPermission anyone can  access
+*
+* @apiHeader {string} => user's access token not implement
+* @apiHeaderExample {json} Header-Example:
+*
+* @apiQuery {id}   [id=1] => id means user id
+*
+*@apiSuccess {Object} Response=> user object
+*
+*@apiError {Unauthorized 401} => only authorized users can access this
+*@apiError {forbidden 403} => only only can access this
+*
+*************/
+router.delete("/delete" , userController.deleteUser);
 
 
 module.exports = router;
